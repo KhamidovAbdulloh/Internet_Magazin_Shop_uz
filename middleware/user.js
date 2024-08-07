125da9cc582bea46efd9446aeeb70351fd80486c
KhamidovAbdulloh <134302308+KhamidovAbdulloh@users.noreply.github.com>
2024-08-07 23:19:49 +0500

    first commit

---
+++ middleware/user.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+export default async function (req, res, next) {
+	if (!req.cookies.token) {
+		next()
+		return
+	}
+
+	const token = req.cookies.token
+	const decode = jwt.verify(token, process.env.JWT_SECRET)
+	if (decode.userId){
+		const user = await User.findById(decode.userId)
+		req.userId = user._id
+	}
+	next()
+}
---
+++ utils/index.js
@@ -0,0 +1,20 @@
+import moment from 'moment'
+
+export default {
+	ifequal(a, b, options) {
+		if (a == b) {
+			return options.fn(this)
+		}
+
+		return options.inverse(this)
+	},
+	getFullNameFirstCharacter(firstName, lastName) {
+		return firstName.charAt(0) + lastName.charAt(0)
+	},
+	getLastNameFirstCharacter(lastName) {
+		return lastName.charAt(0)
+	},
+	formatDate(date) {
+		return moment(date).format('DD MMM, YYYY')
+	},
+}
